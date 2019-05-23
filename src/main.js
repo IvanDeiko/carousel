09931ebc2d4f1122a4a5d2bf79463a4839efc565
Ivan Deiko <ivandeiko1997@gmail.com>
2019-05-23 16:53:51 +0300

    'fix'

--- src/main.js
+++ src/main.js
@@ -2,14 +2,26 @@ const buttonNext = document.querySelector('#next');
 const buttonPrevious = document.querySelector('#previous');
 const container = document.querySelector('#container');
 
+
 buttonNext.addEventListener('click', () => {
-  if (container.scrollLeft === 910) return;
-  container.scrollLeft += 130;
+  buttonPrevious.classList.remove('isDisabled')
+
+  container.scrollLeft += container.clientWidth;
+
+  if ((container.scrollLeft + container.clientWidth) === container.scrollWidth) {
+    buttonNext.classList.add('isDisabled');
+  }
 }) 
 
+
+
 buttonPrevious.addEventListener('click', () => {
-  if(container.scrollLeft === 0) return;
-  container.scrollLeft -= 130;
+  buttonNext.classList.remove('isDisabled');
+  container.scrollLeft -= container.clientWidth;
+
+  if (container.scrollLeft === 0) {
+    buttonPrevious.classList.add('isDisabled')
+  }
 })
 
 console.log(scroll.scrollLeft)
